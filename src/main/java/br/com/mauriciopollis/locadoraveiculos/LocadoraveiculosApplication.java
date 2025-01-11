aadaeff34e3320e40a0e5795a6a2e953aa5e1ac1
mauriciopollis <mauriciopollis@gmail.com>
2025-01-11 15:43:24 -0300

    criando projeto

---
+++ src/main/java/br/com/mauriciopollis/locadoraveiculos/LocadoraveiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.mauriciopollis.locadoraveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraveiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mauriciopollis/locadoraveiculos/LocadoraveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mauriciopollis.locadoraveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
