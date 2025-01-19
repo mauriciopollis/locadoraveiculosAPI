3502e943097e83c7ed1bbd2d16bef1d27fbe9210
mauriciopollis <mauriciopollis@gmail.com>
2025-01-19 08:36:15 -0300

    Aplicando lombok na classe Usuario e incluindo anotações para persistência

--- src/main/java/br/com/mauriciopollis/locadoraveiculos/entity/Usuario.java
+++ src/main/java/br/com/mauriciopollis/locadoraveiculos/entity/Usuario.java
@@ -1,41 +1,25 @@
 package br.com.mauriciopollis.locadoraveiculos.entity;
 
+import br.com.mauriciopollis.locadoraveiculos.entity.enums.TipoUsuarioEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String senha;
+    @Enumerated(EnumType.STRING)
+    private TipoUsuarioEnum tipo;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
---
+++ src/main/java/br/com/mauriciopollis/locadoraveiculos/entity/enums/TipoUsuarioEnum.java
@@ -0,0 +1,6 @@
+package br.com.mauriciopollis.locadoraveiculos.entity.enums;
+
+public enum TipoUsuarioEnum {
+    CLIENTE,
+    GERENTE
+}
