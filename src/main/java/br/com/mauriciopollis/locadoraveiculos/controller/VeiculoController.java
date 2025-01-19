7d82e8ab290082de746de9a0833edbff754f9969
mauriciopollis <mauriciopollis@gmail.com>
2025-01-19 13:23:32 -0300

    colocando anotação do GET

--- src/main/java/br/com/mauriciopollis/locadoraveiculos/controller/VeiculoController.java
+++ src/main/java/br/com/mauriciopollis/locadoraveiculos/controller/VeiculoController.java
@@ -7,10 +7,7 @@ import br.com.mauriciopollis.locadoraveiculos.service.VeiculoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,7 @@ public class VeiculoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createVeiculoResponse);
     }
 
+    @GetMapping
     public ResponseEntity<List<VeiculoResponse>> findAll() {
         List<VeiculoResponse> veiculoResponses = veiculoService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(veiculoResponses);
